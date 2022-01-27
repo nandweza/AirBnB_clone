5d41674f30bc50a4f75f3d461fd3a67254bfb08e
nandweza <allannandweza@gmail.com>
2022-01-26 19:40:19 -0800

    defined the storage method

--- models/base_model.py
+++ models/base_model.py
@@ -23,6 +23,7 @@ class BaseModel:
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
+            storage.new(self)
 
     def __str__(self):
         """string repr of obj"""
