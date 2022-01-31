62af714b8bfde93a46acd5125497c00f144c992a
Dereje Desta <ddesta777@gmail.com>
2022-01-31 22:29:19 +0300

    worked task 10 through 14

--- models/base_model.py
+++ models/base_model.py
@@ -31,7 +31,8 @@ class BaseModel:
 
     def __str__(self):
         """string repr of obj"""
-        return f'[{self.__class__.__name__}] ({self.id}) {self.__dict__}'
+        return '[{}] ({}) {}'.format(self.__class__.__name__,
+                                     self.id, self.__dict__)
 
     def save(self):
         """updates the public instance attribute"""
